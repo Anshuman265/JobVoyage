51c583b944a758dca9a9ea4b790ab92f2cae0521
Anshuman <verma77712@gmail.com>
2024-01-20 19:29:09 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './app/index';
+
+
+const App = () => {
+  return (
+    <Home/>
+  )
+}
+
+
+export default App
+
